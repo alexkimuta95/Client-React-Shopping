aac8373f4c5345a6b57d400e4f66a7bc1eac2eca
Alex Kimutai <billroberts2001@gmail.com>
2019-12-14 11:11:22 +0300

    successful Full stack functionality implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
+import ItemModal from './components/itemModal';
+import { Provider } from 'react-redux';
+import { Container  } from 'reactstrap';
+import store from './store';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppNavbar></AppNavbar>
+      <Container>
+      <ItemModal></ItemModal>
+      <ShoppingList></ShoppingList>
+      </Container>
     </div>
+    </Provider>
   );
 }
 
